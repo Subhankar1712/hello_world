571c820e9e032bf2d2e4a165a2cefda7b17caae8
Subhankar1712 <140783367+Subhankar1712@users.noreply.github.com>
2023-09-01 14:33:55 +0530

    change commit

--- script.bat
+++ script.bat
@@ -1,3 +1,9 @@
 @echo off
 echo Running custom automation...
 REM Add your automation commands here
+
+REM Check for errors and exit with a non-zero status code if necessary
+if ERRORLEVEL 1 (
+    echo Error occurred during automation.
+    exit 1
+)
